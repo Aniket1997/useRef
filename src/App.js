6fcbd2f4f993148ff4e636d255b1572b7b34ef5f
Aniket Paul <16riju@gmail.com>
2023-04-26 00:42:04 +0530

    Server side Pagination

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Projects from './Projects';
-import PhotosTable from './PhotoTable';
+
 
 function App() {
   return (
--- src/Projects.js
+++ src/Projects.js
@@ -4,31 +4,62 @@ import 'bootstrap/dist/css/bootstrap.css';
 import {IoGrid} from "react-icons/io5"
 import { CiViewTable } from "react-icons/ci";
 import { Table } from 'react-bootstrap';
+import { Pagination } from 'react-bootstrap';
+
 function Projects() {
-  const [photos, setPhotos] = useState([]);
-  const [filteredPhotos, setFilteredPhotos] = useState([]);
-  const inputRef = useRef(null);
-  const [viewMode, setViewMode] = useState('grid');
-  useEffect(() => {
-    async function fetchPhotos() {
-      const response = await axios.get('https://jsonplaceholder.typicode.com/photos');
-      setPhotos(response.data);
-      setFilteredPhotos(response.data);
-    }
+    const [photos, setPhotos] = useState([]);
+const [filteredPhotos, setFilteredPhotos] = useState([]);
+const [currentPage, setCurrentPage] = useState(1);
+const [totalPages, setTotalPages] = useState();
+const [paginatedPhotos, setPaginatedPhotos] = useState([]);
+const [perPage, setPerPage] = useState(100); // set to 9 to display 9 photos per page
+const inputRef = useRef(null);
+const [viewMode, setViewMode] = useState('grid');
 
-    fetchPhotos();
-  }, []);
-
-  function handleSearch(e) {
-    e.preventDefault();
-    const searchText = inputRef.current.value;
-    const filteredPhotos = photos.filter(photo => photo.title.toLowerCase().includes(searchText.toLowerCase()));
-    setPhotos(filteredPhotos);
-  }
-  function handleViewModeChange(mode) {
-    setViewMode(mode);
+useEffect(() => {
+  async function fetchPhotos() {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/photos');
+    console.log(response.data);
+    setPhotos(response.data);
+    setFilteredPhotos(response.data.slice(0, perPage));
   }
 
+  fetchPhotos();
+}, [perPage]);
+
+useEffect(() => {
+  const startIndex = 0;
+  const endIndex = startIndex + perPage;
+  const slicedPhotos = filteredPhotos.slice(startIndex, endIndex);
+  setPaginatedPhotos(slicedPhotos);
+  setTotalPages(Math.ceil(filteredPhotos.length / perPage));
+}, [filteredPhotos, perPage]);
+
+function handleSearch(e) {
+  e.preventDefault();
+  const searchText = inputRef.current.value;
+  const filteredPhotos = photos.filter(photo => photo.title.toLowerCase().includes(searchText.toLowerCase()));
+  setFilteredPhotos(filteredPhotos.slice(0, perPage));
+  setCurrentPage(1);
+}
+
+function handleViewModeChange(mode) {
+  setViewMode(mode);
+}
+
+function handlePageChange(pageNumber) {
+  const startIndex = (pageNumber - 1) * perPage;
+  const endIndex = startIndex + perPage;
+  const slicedPhotos = filteredPhotos.slice(startIndex, endIndex);
+  setPaginatedPhotos(slicedPhotos);
+  setCurrentPage(pageNumber);
+}
+
+const pages = [];
+for (let i = 1; i <= totalPages; i++) {
+  pages.push(i);
+}
+  
   return (
     <>
     <nav className="navbar navbar-light bg-light">
@@ -49,7 +80,7 @@ function Projects() {
       {viewMode === 'grid' && (
         <div className="container">
           <div className="row">
-            {filteredPhotos.map(photo => (
+            {paginatedPhotos.map(photo => (
               <div className="col-sm-4" key={photo.id}>
                 <div className="card" style={{ width: "18rem" }}>
                   <img src={photo.url} className="card-img-top" alt={photo.title} style={{ height: "161px", width: "359px" }} />
@@ -63,32 +94,90 @@ function Projects() {
               </div>
             ))}
           </div>
+          <div className="d-flex justify-content-center mt-3">
+          <div className="pagination">
+  <button
+    className="pagination__btn"
+    disabled={currentPage === 1}
+    onClick={() => handlePageChange(currentPage - 1)}
+  >
+    &lt;
+  </button>
+  {pages.map(pageNumber => (
+    <button
+      key={pageNumber}
+      className={`pagination__btn ${currentPage === pageNumber ? 'active' : ''}`}
+      onClick={() => handlePageChange(pageNumber)}
+    >
+      {pageNumber}
+    </button>
+  ))}
+  <button
+    className="pagination__btn"
+    disabled={currentPage === totalPages}
+    onClick={() => handlePageChange(currentPage + 1)}
+  >
+    &gt;
+  </button>
+</div>
+
+          </div>
         </div>
       )}
       {
-        viewMode === 'table' && (
-            <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Image</th>
+  viewMode === 'table' && (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Image</th>
+        </tr>
+      </thead>
+      <tbody>
+        {filteredPhotos.map(photo => (
+          <tr key={photo.id}>
+            <td>{photo.id}</td>
+            <td>{photo.title}</td>
+            <td>
+              <img src={photo.url} alt={photo.title} style={{height:"50px",borderRadius:"50%"}}/>
+            </td>
           </tr>
-        </thead>
-        <tbody>
-          {photos.map(photo => (
-            <tr key={photo.id}>
-              <td>{photo.id}</td>
-              <td>{photo.title}</td>
-              <td>
-                <img src={photo.url} alt={photo.title} style={{height:"50px",borderRadius:"50%"}}/>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-        )
-      }
+        ))}
+      </tbody>
+      <div className="d-flex justify-content-center mt-3">
+          <div className="pagination">
+  <button
+    className="pagination__btn"
+    //disabled={currentPage === 1}
+    onClick={() => handlePageChange(currentPage - 1)}
+  >
+    &lt;
+  </button>
+  {pages.map(pageNumber => (
+    <button
+      key={pageNumber}
+      className={`pagination__btn ${currentPage === pageNumber ? 'active' : ''}`}
+      onClick={() => handlePageChange(pageNumber)}
+    >
+      {pageNumber}
+    </button>
+  ))}
+  <button
+    className="pagination__btn"
+    //disabled={currentPage === totalPages}
+    onClick={() => handlePageChange(currentPage + 1)}
+  >
+    &gt;
+  </button>
+</div>
+
+          </div>
+    </Table>
+    
+  )
+}
+
     </>
   );
 }
